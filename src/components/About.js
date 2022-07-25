791d7de2c09bc7a62340b42af43f223aaafd230b
KevHnh <kevinhinh9@gmail.com>
2022-07-25 19:09:47 -0400

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ import aboutImg2 from '../images/aboutImg2.jpg'
 import './About.css'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faTiktok, faTwitter, faInstagram  } from '@fortawesome/free-brands-svg-icons';
+
 
 function About() {
   return (
@@ -12,18 +15,31 @@ function About() {
             <NavBar/>
         </div>
         <div className="aboutWrapper">
-            <div className="aboutLeftSection">
-                <div className="aboutText">
-                    <div>Hi, my name is Frank.</div>
-                    <div>I have a passion for drawing.</div>
+            <div className="aboutPadding"></div>
+            <div className="aboutCard">
+                <div className="aboutLeftSection">
+                    <div className="aboutText">
+                        <span>Hi, my name is Frank Lu (Flu Sketches).<br/><br/></span>
+                        <span>I draw digitally on Procreate, Adobe photoshop, and Clip studio Paint. 
+                            As artist, we strive to be better and take others around us as role models. 
+                            For me that would include social content creators and modern day artists like 
+                            RossDraws, Guweiz, Wlop, SamdoesArts, and KushinovIlya. Looking at their work 
+                            inspires me to do better and to be able to tell stories or messages in a single 
+                            picture.</span>
+                    </div>
+                    <div className="socialMediaContainer">
+                        <a href="https://twitter.com/NFT_Flu" target="_blank"><FontAwesomeIcon className="socialIcons" icon={faTwitter} style={{fontSize: '2vw'}}/></a>
+                        <a href="https://www.instagram.com/flu_sketches/" target="_blank"><FontAwesomeIcon className="socialIcons" icon={faInstagram} style={{fontSize: '2vw'}}/></a>
+                        <a href="https://www.tiktok.com/@flu_sketches?lang=en" target="_blank"><FontAwesomeIcon className="socialIcons" icon={faTiktok} style={{fontSize: '2vw'}}/></a>
+                    </div>
+                </div>
+                <div className="aboutRightSection">
+                    <div className="rightSectionItems">
+                        <LazyLoadImage effect="blur" className="aboutImgContainer" alt="" src={aboutImg2}/>
+                    </div>
                 </div>
             </div>
-            <div className="aboutRightSection">
-                <div className="rightSectionItems">
-                    <LazyLoadImage effect="blur" className="aboutImgContainer" alt="" src={aboutImg2}/>
-                    <div className="aboutImgDesc">Flu Sketches</div>
-                </div>
-            </div>
+            <div className="aboutPadding"></div>
         </div>
 
     </div>
